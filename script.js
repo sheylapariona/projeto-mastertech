78afeefd9e4b90d6398cd237b56267a12d4a6680
sheylapariona <sheylapariona@gmail.com>
2018-01-23 22:46:32 -0200

    ajustes em aula

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let titulo = document.querySelector('h1');
 let texto = document.querySelector('p');
 
+
 function mudarTitulo() {
-  titulo.innerHTML = 'Sua cidade';
+  titulo.innerHTML = "Sua cidade";
+  titulo.style.backgroundColor = "red";
+  titulo.style.color = "white"
 }
 
 function mudarTexto() {
-  texto.style.color = 'red';
+  texto.style.color = "white";
+  texto.style.backgroundColor = "red";
 }
